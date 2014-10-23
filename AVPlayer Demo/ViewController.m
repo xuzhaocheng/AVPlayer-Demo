75f19738c306bbf60e60c831ce341ab3265d8e84
xuzhaocheng <zhaochengxu1991@gmail.com>
2014-10-23 16:16:35 +0800

    Initial Commit

---
+++ AVPlayer Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AVPlayer Demo
+//
+//  Created by xuzhaocheng on 14/10/23.
+//  Copyright (c) 2014年 Zhejiang University. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AVPlayer Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AVPlayer Demo
+//
+//  Created by xuzhaocheng on 14/10/23.
+//  Copyright (c) 2014年 Zhejiang University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
